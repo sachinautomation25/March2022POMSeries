0795e798909e0f583e12ba3fe7ce6d4359715872
Sachin Sharma <94907192+sachinautomation25@users.noreply.github.com>
2022-03-15 09:25:15 +0530

    added SignUpPage

---
+++ src/main/java/com/qa/trcrm/pages/SignUpPage.java
@@ -0,0 +1,14 @@
+package com.qa.trcrm.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.qa.trcrm.base.BasePage;
+
+public class SignUpPage extends BasePage {
+
+	WebDriver driver;
+	public SignUpPage(WebDriver driver)
+	{
+		this.driver=driver;
+	}
+}
